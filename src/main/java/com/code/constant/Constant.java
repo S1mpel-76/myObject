75e3c97bff6dc0f82ef5ae1ca743039982d48617
‘lihaoze97’ <931281769@qq.com>
2022-07-09 14:55:19 +0800

    代码生成器

---
+++ src/main/java/com/code/constant/Constant.java
@@ -0,0 +1,14 @@
+package com.code.constant;
+
+/**
+ * Created by zezhong.shang on 17-6-5.
+ */
+public class Constant {
+
+    public static String template_path = "D:\\Study\\code-parent\\src\\main\\resources\\itrip";
+
+    public static String service_save_path = "D://workspace//itrip//service";
+    public static String servie_impl_save_path = "D://workspace//itrip//service";
+    public static String mapper_save_path = "D://workspace//itrip//mapper";
+    public static String model_save_path = "D://workspace//itrip//model";
+}
